76f0766fea7cfcfd471f1971ff8d23a947ed26b1
Prof. Lander <1253239+ProfLander@users.noreply.github.com>
2025-12-24 04:51:52 +0000

    Upgrade to NixOS 25.11

--- nixos/flake.nix
+++ nixos/flake.nix
@@ -1,12 +1,12 @@
 {
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.05";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.11";
     nix-gaming.url = "github:fufexan/nix-gaming";
     niri-flake.url = "git+file:///home/lander/src/niri-flake";
     niri.url = "git+file:///home/lander/src/niri";
     #niri.url = "github:YaLTeR/niri";
   };
-  outputs = inputs@{ self, nixpkgs, ... }: {
+  outputs = inputs@{ nixpkgs, ... }: {
     nixosConfigurations.artemis = nixpkgs.lib.nixosSystem {
       system = "x86_64-linux";
       specialArgs = {inherit inputs;};
