9792c2e1f38641dd3a5b44de6e4b01bc7dde9b39
Prof. Lander <1253239+ProfLander@users.noreply.github.com>
2025-12-24 04:51:38 +0000

    Minor syntax fix

--- nixos/asterix/packages.nix
+++ nixos/asterix/packages.nix
@@ -4,7 +4,7 @@
 
   # Enable Cachix for nix-gaming
   nix.settings = {
-    substituters = ["https://nix-gaming.cachix.org"];
-    trusted-public-keys = ["nix-gaming.cachix.org-1:nbjlureqMbRAxR1gJ/f3hxemL9svXaZF/Ees8vCUUs4="];
+    substituters = [ "https://nix-gaming.cachix.org" ];
+    trusted-public-keys = [ "nix-gaming.cachix.org-1:nbjlureqMbRAxR1gJ/f3hxemL9svXaZF/Ees8vCUUs4=" ];
   };
 }
