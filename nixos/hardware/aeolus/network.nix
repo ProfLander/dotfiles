43f180d557e5a28b19af06558563a68f58eef0fe
Prof. Lander <1253239+ProfLander@users.noreply.github.com>
2026-03-12 01:30:36 +0000

    Setup static IP addresses

--- nixos/hardware/aeolus/network.nix
+++ nixos/hardware/aeolus/network.nix
@@ -1,15 +1,21 @@
-{ lib, ... }:
-
 {
   networking = {
     hostName = "aeolus";
-    useDHCP = lib.mkDefault true;
+    useDHCP = false;
 
     interfaces = {
       enp2s0 = {
+        ipv4.addresses = [
+          {
+            address = "192.168.1.2";
+            prefixLength = 24;
+          }
+        ];
+
         wakeOnLan.enable = true;
       };
     };
+
     firewall = {
       allowedUDPPorts = [ 9 ];
     };
--- nixos/hardware/artemis/network.nix
+++ nixos/hardware/artemis/network.nix
@@ -1,6 +1,19 @@
 { lib, ... }:
 
 {
-  networking.hostName = "artemis";
-  networking.useDHCP = lib.mkDefault true;
+  networking = {
+    hostName = "artemis";
+    useDHCP = false;
+
+    interfaces = {
+      enp6s0 = {
+        ipv4.addresses = [
+          {
+            address = "192.168.1.3";
+            prefixLength = 24;
+          }
+        ];
+      };
+    };
+  };
 }
