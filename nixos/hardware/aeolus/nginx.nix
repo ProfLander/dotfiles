06ec3356bef2ccd010d655e202fe3c943d8db601
Prof. Lander <1253239+ProfLander@users.noreply.github.com>
2026-03-14 18:20:57 +0000

    Iterate on nginx config

--- nixos/hardware/aeolus/nginx.nix
+++ nixos/hardware/aeolus/nginx.nix
@@ -1,7 +1,7 @@
 {
   services.nginx = {
     enable = true;
-    virtualHosts.localhost = {
+    virtualHosts."prof-lander.duckdns.org" = {
       locations."/" = {
         return = "200 '<html><body>It works</body></html>'";
         extraConfig = ''
@@ -9,5 +9,18 @@
         '';
       };
     };
+  };
+
+  networking.firewall.allowedTCPPorts = [
+    80
+    443
+  ];
+
+  security.acme = {
+    # Accept the CA’s terms of service. The default provider is Let’s Encrypt, you can find their ToS at https://letsencrypt.org/repository/.
+    acceptTerms = true;
+    # Optional: You can configure the email address used with Let's Encrypt.
+    # This way you get renewal reminders (automated by NixOS) as well as expiration emails.
+    defaults.email = "jpalmerwatkins@gmail.com";
   };
 }
