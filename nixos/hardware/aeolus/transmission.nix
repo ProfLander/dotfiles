fdd4ced8055ad7e4eb6a4a84f9a76e4fcb90baa6
Prof. Lander <1253239+ProfLander@users.noreply.github.com>
2026-03-14 19:00:59 +0000

    Setup local transmission web interface

--- nixos/hardware/aeolus/transmission.nix
+++ nixos/hardware/aeolus/transmission.nix
@@ -1,3 +1,15 @@
 {
-  services.transmission.enable = true;
+  services.transmission = {
+    enable = true;
+    settings = {
+      rpc-bind-address = "0.0.0.0"; # Allows access from other devices
+      rpc-port = 9091;
+      rpc-authentication-required = false;
+      rpc-whitelist = "*";
+    };
+  };
+
+  # Open firewall ports
+  networking.firewall.allowedTCPPorts = [ 9091 51413 ];
+  networking.firewall.allowedUDPPorts = [ 51413 ];
 }
