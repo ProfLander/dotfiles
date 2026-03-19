ea18f5540dcabac8f11b523b63f5338a3fc1b485
Prof. Lander <1253239+ProfLander@users.noreply.github.com>
2026-03-19 15:52:00 +0000

    Add calliope config, initial home-manager setup

--- nixos/flake.nix
+++ nixos/flake.nix
@@ -1,15 +1,39 @@
 {
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.11";
+
     nix-gaming.url = "github:fufexan/nix-gaming";
+
     niri-flake.url = "github:ProfLander/niri-flake";
     niri.url = "github:ProfLander/niri";
     #niri.url = "github:YaLTeR/niri";
+
+    home-manager.url = "github:nix-community/home-manager";
+    home-manager.inputs.nixpkgs.follows = "nixpkgs";
   };
+
   outputs =
-    inputs@{ nixpkgs, ... }:
+    inputs@{
+      nixpkgs,
+      home-manager,
+      ...
+    }:
     {
       nixosConfigurations = {
+        # Mini PC
+        calliope = nixpkgs.lib.nixosSystem {
+          system = "x86_64-linux";
+          specialArgs = { inherit inputs; };
+          modules = [
+            ./hardware/calliope/default.nix
+            home-manager.nixosModules.home-manager
+            {
+              home-manager.useGlobalPkgs = true;
+              home-manager.useUserPackages = true;
+            }
+          ];
+        };
+
         # Desktop
         artemis = nixpkgs.lib.nixosSystem {
           system = "x86_64-linux";
