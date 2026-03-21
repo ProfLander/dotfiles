2160406207e8f4ce8f65c6c7b14b25834af2d4f4
Prof. Lander <1253239+ProfLander@users.noreply.github.com>
2026-03-21 16:50:26 +0000

    Drop Gigabyte monitor to ~60hz for calliope

--- hardware/calliope/home.nix
+++ hardware/calliope/home.nix
@@ -6,7 +6,7 @@
         mode = {
           width = 3840;
           height = 2160;
-          refresh = 119.910;
+          refresh = 59.997;
         };
         variable-refresh-rate = true;
         scale = 1.5;
--- hardware/calliope/monitors.nix
+++ hardware/calliope/monitors.nix
@@ -1,7 +1,7 @@
 {
   # Set monitor video modes
   boot.kernelParams = [
-    "video=DP-1:3840x2160@120"
+    "video=DP-1:3840x2160@60"
     "video=DP-2:2560x1440@60"
     "video=DP-4:1920x1200@60"
   ];
