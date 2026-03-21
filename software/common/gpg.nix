aec03776f6faf9e9d89afc7d8c7d2248ab192d29
Prof. Lander <1253239+ProfLander@users.noreply.github.com>
2026-03-21 17:13:09 +0000

    Set pinentry-tty in gpg module

--- software/common/gpg.nix
+++ software/common/gpg.nix
@@ -1,7 +1,9 @@
+{ lib, pkgs, ... }:
 {
   # Enable GPG agent with SSH support
   programs.gnupg.agent = {
     enable = true;
     enableSSHSupport = true;
+    pinentryPackage = pkgs.pinentry-tty;
   };
 }
