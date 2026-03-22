3c43d6ccd56459a68daaa96f66f1ba84ba7d4048
Prof. Lander <1253239+ProfLander@users.noreply.github.com>
2026-03-22 01:54:12 +0000

    Integrate MacTahoe-Dark GTK theme

--- software/desktop/gtk.nix
+++ software/desktop/gtk.nix
@@ -1,9 +1,32 @@
 { pkgs, ... }:
 
-{
+let
+  stdenv = pkgs.stdenv;
+  fetchFromGitHub = pkgs.fetchFromGitHub;
+
+  mac-tahoe = stdenv.mkDerivation rec {
+    pname = "MacTahoe-Dark";
+    version = "a678d53eb070823df724fd8a6fac502106a4702d";
+    dontBuild = true;
+
+    src = fetchFromGitHub {
+      owner = "ProfLander";
+      repo = "MacTahoe-gtk-theme";
+      rev = "${version}";
+      sha256 = "sha256-NdKeI4q1M3Z9DGJy73n4okjk6W7Qru0E1Ka0C6bmyko=";
+    };
+
+    installPhase = ''
+      tar -xvf $src/release/${pname}.tar.xz
+      mkdir -p $out/share/themes
+      cp -aR ${pname} $out/share/themes/${pname}
+    '';
+  };
+in {
   # Use GTK3 for color / emoji / file pickers
   environment.systemPackages = [
     pkgs.gtk3
+    mac-tahoe
   ];
 
   # Override the GSettings schema dir to point at gtk3
