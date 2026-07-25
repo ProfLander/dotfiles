6f972c04f171bbce6fb0cb12452df1f76e5a351c
Prof. Lander <1253239+ProfLander@users.noreply.github.com>
2026-07-25 15:23:50 +0100

    nvim: Reimplement tree-sitter autostart

--- home/lander/cli/text-editor/lander-nvim/fnl/lander/nvim/tree-sitter.fnl
+++ home/lander/cli/text-editor/lander-nvim/fnl/lander/nvim/tree-sitter.fnl
@@ -1,4 +1,4 @@
-(local treesitter (require :nvim-treesitter))
-
-(treesitter.setup {:highlight {:enable true
-                               :additional_vim_regex_highlighting false}})
+(vim.api.nvim_create_autocmd :FileType
+  {:pattern "*"
+   :callback (fn [args]
+               (pcall vim.treesitter.start args.buf))})
--- home/lander/cli/text-editor/lander-nvim/lua/lander/nvim/tree-sitter.lua
+++ home/lander/cli/text-editor/lander-nvim/lua/lander/nvim/tree-sitter.lua
@@ -1,4 +1,5 @@
 -- [nfnl] fnl/lander/nvim/tree-sitter.fnl
- local treesitter = require("nvim-treesitter")
 
- return treesitter.setup({highlight = {enable = true, additional_vim_regex_highlighting = false}})
+
+ local function _1_(args)
+ return pcall(vim.treesitter.start, args.buf) end return vim.api.nvim_create_autocmd("FileType", {pattern = "*", callback = _1_})
