e0b8f3da1988e86239dc0b01cb360445cec35e75
Prof. Lander <1253239+ProfLander@users.noreply.github.com>
2026-07-25 20:39:45 +0100

    Switch artemis back to UUID disks

--- hardware/artemis/disks.nix
+++ hardware/artemis/disks.nix
@@ -1,11 +1,11 @@
 {
   fileSystems."/" = {
-    device = "/dev/nvme0n1p2";
+    device = "/dev/disk/by-uuid/847eb04d-4acf-4f1c-88d5-701b6a62019c";
     fsType = "ext4";
   };
 
   fileSystems."/boot" = {
-    device = "/dev/nvme0n1p1";
+    device = "/dev/disk/by-uuid/0F22-6821";
     fsType = "vfat";
     options = [
       "fmask=0077"
@@ -14,22 +14,22 @@
   };
 
   fileSystems."/mnt/backup" = {
-    device = "/dev/sda1";
+    device = "/dev/disk/by-uuid/1d53e828-6d5b-4b97-8627-84e4adb31391";
     fsType = "ext4";
   };
 
   fileSystems."/mnt/media" = {
-    device = "/dev/sdb1";
+    device = "/dev/disk/by-uuid/c4138465-5c7b-4164-9404-90a1ceef0d77";
     fsType = "ext4";
   };
 
   fileSystems."/mnt/scratch" = {
-    device = "/dev/sdc1";
+    device = "/dev/disk/by-uuid/9bec8d1d-a0d8-4f88-b2dc-566dbb479b7c";
     fsType = "btrfs";
   };
 
   fileSystems."/mnt/projects" = {
-    device = "/dev/sdd1";
+    device = "/dev/disk/by-uuid/9c0d33e7-4695-4a4a-a1d3-3724a76f8e91";
     fsType = "ext4";
   };
 
