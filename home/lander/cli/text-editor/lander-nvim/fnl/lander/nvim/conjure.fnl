a1388f7975e2df55f0201a684922ac2d0bf5dc38
Prof. Lander <1253239+ProfLander@users.noreply.github.com>
2026-07-25 01:36:56 +0100

    conjure: Don't autostart REPL or override C-k

--- home/lander/cli/text-editor/lander-nvim/fnl/lander/nvim/conjure.fnl
+++ home/lander/cli/text-editor/lander-nvim/fnl/lander/nvim/conjure.fnl
@@ -1,9 +1,14 @@
+;; Don't autostart REPL on opening a file
+(set (. vim :g "conjure#client_on_load") false)
+
+;; Don't override C-k with eval-current-form
+(set (. vim :g "conjure#mapping#doc_word") false)
+
 ;; Use Fennel stdio backend by default
 (set (. vim :g "conjure#filetype#fennel") "conjure.client.fennel.stdio")
 
 ;; Setup Fennel stdio command
  (set (. vim :g "conjure#client#fennel#stdio#command") "fennel")
-; (set (. vim :g "conjure#client#fennel#stdio#command") "duck-repl")
 
 ;; Chicken Scheme
 (set (. vim :g "conjure#client#scheme#stdio#command") "csi -:c")
--- home/lander/cli/text-editor/lander-nvim/lua/lander/nvim/conjure.lua
+++ home/lander/cli/text-editor/lander-nvim/lua/lander/nvim/conjure.lua
@@ -1,5 +1,10 @@
 -- [nfnl] fnl/lander/nvim/conjure.fnl
- vim["g"]["conjure#filetype#fennel"] = "conjure.client.fennel.stdio" vim["g"]["conjure#client#fennel#stdio#command"] = "fennel" vim["g"]["conjure#client#scheme#stdio#command"] = "csi -:c" vim["g"]["conjure#client#scheme#stdio#prompt_pattern"] = "#;%d>" vim["g"]["conjure#client#scheme#stdio#value_prefix_pattern"] = false
+ vim["g"]["conjure#client_on_load"] = false vim["g"]["conjure#mapping#doc_word"] = false vim["g"]["conjure#filetype#fennel"] = "conjure.client.fennel.stdio" vim["g"]["conjure#client#fennel#stdio#command"] = "fennel" vim["g"]["conjure#client#scheme#stdio#command"] = "csi -:c" vim["g"]["conjure#client#scheme#stdio#prompt_pattern"] = "#;%d>" vim["g"]["conjure#client#scheme#stdio#value_prefix_pattern"] = false
+
+
+
+
+
 
 
 
